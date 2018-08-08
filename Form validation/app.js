4811b219291094d41123baaf9ad329c97974130e
zhutto <35507425+zhutto@users.noreply.github.com>
2018-08-08 17:02:43 -0600

    Add files via upload

--- Form validation/app.js
+++ Form validation/app.js
@@ -31,9 +31,9 @@ function validateZip() {
 
 function validateEmail() {
   const email = document.getElementById('email');
-  const re = /^[0-9]{5}(-[0-9]{4})$/;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2-5})$/;
 
-  if(!re.test(zip.value)){
+  if(!re.test(email.value)){
   email.classList.add('is-invalid');
   } else {
   email.classList.remove('is-invalid');
@@ -42,11 +42,11 @@ function validateEmail() {
 
 function validatePhone() {
   const phone = document.getElementById('phone');
-  const re = /^[a-zA-Z]{2,10}$/;
+  const re = /^\(?d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
 
-  if(!re.test(name.value)){
-    name.classList.add('is-invalid');
+  if(!re.test(phone.value)){
+    phone.classList.add('is-invalid');
   } else {
-    name.classList.remove('is-invalid');
+    phone.classList.remove('is-invalid');
   }
 }
